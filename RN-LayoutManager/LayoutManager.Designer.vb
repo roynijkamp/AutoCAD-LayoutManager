194572bf1ea82cd60b0dd6cace1d06d5bc66aea7
ANACON\RoyN <roynijkamp@roynijkamp.nl>
2017-10-20 12:32:34 +0200

    Working Example

---
+++ RN-LayoutManager/LayoutManager.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class LayoutManager
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.flowLayouts = New System.Windows.Forms.FlowLayoutPanel()
+        Me.lblTitel = New System.Windows.Forms.Label()
+        Me.cmdRefreshList = New System.Windows.Forms.Button()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 1
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.flowLayouts, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblTitel, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.cmdRefreshList, 0, 2)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 3
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(420, 956)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'flowLayouts
+        '
+        Me.flowLayouts.AutoScroll = True
+        Me.flowLayouts.AutoSize = True
+        Me.flowLayouts.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.flowLayouts.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.flowLayouts.FlowDirection = System.Windows.Forms.FlowDirection.TopDown
+        Me.flowLayouts.Location = New System.Drawing.Point(3, 23)
+        Me.flowLayouts.Name = "flowLayouts"
+        Me.flowLayouts.Size = New System.Drawing.Size(414, 890)
+        Me.flowLayouts.TabIndex = 0
+        Me.flowLayouts.WrapContents = False
+        '
+        'lblTitel
+        '
+        Me.lblTitel.AutoSize = True
+        Me.lblTitel.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.lblTitel.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lblTitel.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitel.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.lblTitel.Location = New System.Drawing.Point(3, 3)
+        Me.lblTitel.Margin = New System.Windows.Forms.Padding(3, 3, 3, 0)
+        Me.lblTitel.Name = "lblTitel"
+        Me.lblTitel.Size = New System.Drawing.Size(414, 17)
+        Me.lblTitel.TabIndex = 1
+        Me.lblTitel.Text = "RN Layout Manager"
+        '
+        'cmdRefreshList
+        '
+        Me.cmdRefreshList.Location = New System.Drawing.Point(3, 919)
+        Me.cmdRefreshList.Name = "cmdRefreshList"
+        Me.cmdRefreshList.Size = New System.Drawing.Size(44, 34)
+        Me.cmdRefreshList.TabIndex = 2
+        Me.cmdRefreshList.Text = "REF"
+        Me.cmdRefreshList.UseVisualStyleBackColor = True
+        '
+        'LayoutManager
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Name = "LayoutManager"
+        Me.Size = New System.Drawing.Size(420, 956)
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TableLayoutPanel1 As Windows.Forms.TableLayoutPanel
+    Friend WithEvents flowLayouts As Windows.Forms.FlowLayoutPanel
+    Friend WithEvents lblTitel As Windows.Forms.Label
+    Friend WithEvents cmdRefreshList As Windows.Forms.Button
+End Class
--- RN-LayoutManager/myCommands.vb
+++ RN-LayoutManager/myCommands.vb
@@ -1,4 +1,4 @@
-﻿' (C) Copyright 2011 by  
+﻿' (C) Copyright 2017 by  Roy Nijkamp
 '
 Imports System
 Imports Autodesk.AutoCAD.Runtime
@@ -6,70 +6,32 @@ Imports Autodesk.AutoCAD.ApplicationServices
 Imports Autodesk.AutoCAD.DatabaseServices
 Imports Autodesk.AutoCAD.Geometry
 Imports Autodesk.AutoCAD.EditorInput
+Imports Autodesk.AutoCAD.Windows
+Imports Autodesk.AutoCAD.Windows.ToolPalette
 
 ' This line is not mandatory, but improves loading performances
 <Assembly: CommandClass(GetType(RN_LayoutManager.MyCommands))>
 Namespace RN_LayoutManager
 
-    ' This class is instantiated by AutoCAD for each document when
-    ' a command is called by the user the first time in the context
-    ' of a given document. In other words, non static data in this class
-    ' is implicitly per-document!
     Public Class MyCommands
-
-        ' The CommandMethod attribute can be applied to any public  member 
-        ' function of any public class.
-        ' The function should take no arguments and return nothing.
-        ' If the method is an instance member then the enclosing class is 
-        ' instantiated for each document. If the member is a static member then
-        ' the enclosing class is NOT instantiated.
-        '
-        ' NOTE: CommandMethod has overloads where you can provide helpid and
-        ' context menu.
-
-        ' Modal Command with localized name
-        ' AutoCAD will search for a resource string with Id "MyCommandLocal" in the 
-        ' same namespace as this command class. 
-        ' If a resource string is not found, then the string "MyLocalCommand" is used 
-        ' as the localized command name.
-        ' To view/edit the resx file defining the resource strings for this command, 
-        ' * click the 'Show All Files' button in the Solution Explorer;
-        ' * expand the tree node for myCommands.vb;
-        ' * and double click on myCommands.resx
-        <CommandMethod("MyGroup", "MyCommand", "MyCommandLocal", CommandFlags.Modal)> _
-        Public Sub MyCommand() ' This method can have any name
-            ' Put your command code here
-        End Sub
-
-        ' Modal Command with pickfirst selection
-        <CommandMethod("MyGroup", "MyPickFirst", "MyPickFirstLocal", CommandFlags.Modal + CommandFlags.UsePickSet)> _
-        Public Sub MyPickFirst() ' This method can have any name
-            Dim result As PromptSelectionResult = Application.DocumentManager.MdiActiveDocument.Editor.GetSelection()
-            If (result.Status = PromptStatus.OK) Then
-                ' There are selected entities
-                ' Put your command using pickfirst set code here
-            Else
-                ' There are no selected entities
-                ' Put your command code here
-            End If
-        End Sub
+        Friend Shared m_palette As Autodesk.AutoCAD.Windows.PaletteSet = Nothing
 
         ' Application Session Command with localized name
-        <CommandMethod("MyGroup", "MySessionCmd", "MySessionCmdLocal", CommandFlags.Modal + CommandFlags.Session)> _
-        Public Sub MySessionCmd() ' This method can have any name
-            ' Put your command code here
+        <CommandMethod("layoutman", CommandFlags.Modal + CommandFlags.Session)>
+        Public Sub layoutman()
+            If m_palette Is Nothing Then
+                'palette nog niet geopend
+                m_palette = New Autodesk.AutoCAD.Windows.PaletteSet("RN-LayoutManager", New Guid("{fad7b1e9-625e-4c10-9ba4-c94681a982cf}"))
+                m_palette.Style = PaletteSetStyles.ShowPropertiesMenu + PaletteSetStyles.ShowAutoHideButton + PaletteSetStyles.ShowCloseButton
+                Dim palette_overlayman As LayoutManager = New LayoutManager()
+                m_palette.Add("RN-LayoutManager", palette_overlayman)
+
+            End If
+            'm_palette.Icon = GetEmbeddedIcon("aitoolsicon.ico")
+            'palette aanzetten
+            m_palette.Visible = True
         End Sub
 
-        ' LispFunction is similar to CommandMethod but it creates a lisp 
-        ' callable function. Many return types are supported not just string
-        ' or integer.
-        <LispFunction("MyLispFunction", "MyLispFunctionLocal")> _
-        Public Function MyLispFunction(ByVal args As ResultBuffer) ' This method can have any name
-            ' Put your command code here
-
-            ' Return a value to the AutoCAD Lisp Interpreter
-            Return 1
-        End Function
 
     End Class
 
