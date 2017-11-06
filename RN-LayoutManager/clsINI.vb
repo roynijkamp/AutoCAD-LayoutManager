00f99f1fd1e02567b87a0108cf4d08262a94158e
royn <royn@ANACON15>
2017-11-06 13:25:13 +0100

    start settings

---
+++ RN-LayoutManager/clsINI.vb
@@ -0,0 +1,66 @@
+﻿Imports Microsoft.VisualBasic
+Imports Microsoft.VisualBasic.CompilerServices
+Imports System
+Imports System.Runtime.InteropServices
+Imports System.Text
+
+Public Class clsINI
+    Private strFilename As String
+
+    Public ReadOnly Property FileName() As String
+        Get
+            Return Me.strFilename
+        End Get
+    End Property
+
+    Private Declare Ansi Function GetPrivateProfileString Lib "kernel32.dll" Alias "GetPrivateProfileStringA" (<MarshalAs(UnmanagedType.VBByRefStr)> ByRef lpApplicationName As String, <MarshalAs(UnmanagedType.VBByRefStr)> ByRef lpKeyName As String, <MarshalAs(UnmanagedType.VBByRefStr)> ByRef lpDefault As String, lpReturnedString As StringBuilder, nSize As Integer, <MarshalAs(UnmanagedType.VBByRefStr)> ByRef lpFileName As String) As Integer
+
+    Private Declare Ansi Function WritePrivateProfileString Lib "kernel32.dll" Alias "WritePrivateProfileStringA" (<MarshalAs(UnmanagedType.VBByRefStr)> ByRef lpApplicationName As String, <MarshalAs(UnmanagedType.VBByRefStr)> ByRef lpKeyName As String, <MarshalAs(UnmanagedType.VBByRefStr)> ByRef lpString As String, <MarshalAs(UnmanagedType.VBByRefStr)> ByRef lpFileName As String) As Integer
+
+    Private Declare Ansi Function GetPrivateProfileInt Lib "kernel32.dll" Alias "GetPrivateProfileIntA" (<MarshalAs(UnmanagedType.VBByRefStr)> ByRef lpApplicationName As String, <MarshalAs(UnmanagedType.VBByRefStr)> ByRef lpKeyName As String, nDefault As Integer, <MarshalAs(UnmanagedType.VBByRefStr)> ByRef lpFileName As String) As Integer
+
+    Private Declare Ansi Function FlushPrivateProfileString Lib "kernel32.dll" Alias "WritePrivateProfileStringA" (lpApplicationName As Integer, lpKeyName As Integer, lpString As Integer, <MarshalAs(UnmanagedType.VBByRefStr)> ByRef lpFileName As String) As Integer
+
+    Public Sub New(Filename As String)
+        Me.strFilename = Filename
+    End Sub
+
+    Public Function GetString(Section As String, Key As String, [Default] As String) As String
+        Dim stringBuilder As StringBuilder = New StringBuilder(256)
+        Dim privateProfileString As Integer = clsINI.GetPrivateProfileString(Section, Key, [Default], stringBuilder, stringBuilder.Capacity, Me.strFilename)
+        Dim result As String
+        If privateProfileString > 0 Then
+            result = Strings.Left(stringBuilder.ToString(), privateProfileString)
+        Else
+            result = ""
+        End If
+        Return result
+    End Function
+
+    Public Function GetInteger(Section As String, Key As String, [Default] As Integer) As Integer
+        Return clsINI.GetPrivateProfileInt(Section, Key, [Default], Me.strFilename)
+    End Function
+
+    Public Function GetBoolean(Section As String, Key As String, [Default] As Boolean) As Boolean
+        Return clsINI.GetPrivateProfileInt(Section, Key, If((-(If(([Default] > False), 1, 0))), 1, 0), Me.strFilename) = 1
+    End Function
+
+    Public Sub WriteString(Section As String, Key As String, Value As String)
+        clsINI.WritePrivateProfileString(Section, Key, Value, Me.strFilename)
+        Me.Flush()
+    End Sub
+
+    Public Sub WriteInteger(Section As String, Key As String, Value As Integer)
+        Me.WriteString(Section, Key, Conversions.ToString(Value))
+        Me.Flush()
+    End Sub
+
+    Public Sub WriteBoolean(Section As String, Key As String, Value As Boolean)
+        Me.WriteString(Section, Key, Conversions.ToString(If((-(If((Value = True), 1, 0))), 1, 0)))
+        Me.Flush()
+    End Sub
+
+    Private Sub Flush()
+        clsINI.FlushPrivateProfileString(0, 0, 0, Me.strFilename)
+    End Sub
+End Class
--- RN-LayoutManager/ucSettings.vb
+++ RN-LayoutManager/ucSettings.vb
@@ -1,16 +1,26 @@
 ﻿Imports System.IO
+Imports System.Reflection
 Imports System.Windows.Forms
 
 Public Class ucSettings
+    Dim sIniDir As String = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & "\RNtools"
+    Dim sIniFile As String = "\layoutmanager.ini"
+    Dim iniFile As clsINI
+    Dim sPDFuserFolder As String = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+    Dim sDefaultOutputLocation As String = "drawingfolder"
+    Dim sCurrVersion As String = Assembly.GetExecutingAssembly().GetName().Version.ToString
     Private Sub radioPDFuserFolder_CheckedChanged(sender As Object, e As EventArgs) Handles radioPDFuserFolder.CheckedChanged
         If radioPDFuserFolder.Checked Then
             txtUserSaveFolder.Enabled = True
             cmdSelectUserSaveFolder.Enabled = True
+            sDefaultOutputLocation = "userlocation"
+            saveDefaultOutput()
         Else
             txtUserSaveFolder.Enabled = False
             cmdSelectUserSaveFolder.Enabled = False
         End If
     End Sub
+
 
     Private Sub cmdSelectUserSaveFolder_Click(sender As Object, e As EventArgs) Handles cmdSelectUserSaveFolder.Click
         Using fldrDia As FolderBrowserDialog = New FolderBrowserDialog
@@ -18,8 +28,57 @@ Public Class ucSettings
                 fldrDia.SelectedPath = txtUserSaveFolder.Text
             End If
             If fldrDia.ShowDialog() = DialogResult.OK Then
-                txtUserSaveFolder.Text = fldrDia.SelectedPath
+                'eerst check of map wel bestaat
+                If My.Computer.FileSystem.DirectoryExists(sIniDir) = False Then
+                    My.Computer.FileSystem.CreateDirectory(sIniDir)
+                End If
+                sPDFuserFolder = fldrDia.SelectedPath
+                txtUserSaveFolder.Text = sPDFuserFolder
+                'bestand aanmaken
+                'settings schrijven
+                iniFile = New clsINI(sIniDir & sIniFile)
+                iniFile.WriteString("publishsettings", "outputfolder", sPDFuserFolder)
             End If
         End Using
+    End Sub
+
+    Private Sub ucSettings_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'check of ini bestand bestaat, zo ja: instellingen laden
+        If File.Exists(sIniDir & sIniFile) Then
+            'bestand bestaat, instelingen laden
+            iniFile = New clsINI(sIniDir & sIniFile)
+            sPDFuserFolder = iniFile.GetString("publishsettings", "outputfolder", sPDFuserFolder)
+            txtUserSaveFolder.Text = sPDFuserFolder
+            sDefaultOutputLocation = iniFile.GetString("publishsettings", "defaultoutput", sDefaultOutputLocation)
+            Select Case sDefaultOutputLocation
+                Case "drawingfolder"
+                    radioPDFdrawingFolder.Checked = True
+                Case "userlocation"
+                    radioPDFuserFolder.Checked = True
+                Case "askonplot"
+                    radioPDFfolderAsk.Checked = True
+            End Select
+        Else
+            'eerst check of map wel bestaat
+            If My.Computer.FileSystem.DirectoryExists(sIniDir) = False Then
+                My.Computer.FileSystem.CreateDirectory(sIniDir)
+                iniFile = New clsINI(sIniDir & sIniFile)
+                iniFile.WriteString("appsettings", "version", sCurrVersion)
+            End If
+        End If
+    End Sub
+
+    Private Sub radioPDFfolderAsk_CheckedChanged(sender As Object, e As EventArgs) Handles radioPDFfolderAsk.CheckedChanged
+        sDefaultOutputLocation = "askonplot"
+        saveDefaultOutput()
+    End Sub
+
+    Private Sub radioPDFdrawingFolder_CheckedChanged(sender As Object, e As EventArgs) Handles radioPDFdrawingFolder.CheckedChanged
+        sDefaultOutputLocation = "drawingfolder"
+        saveDefaultOutput()
+    End Sub
+    Public Sub saveDefaultOutput()
+        iniFile = New clsINI(sIniDir & sIniFile)
+        iniFile.WriteString("publishsettings", "defaultoutput", sDefaultOutputLocation)
     End Sub
 End Class
